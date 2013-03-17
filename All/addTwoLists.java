1fe8fcf7261a5d82ab616306598fa9cc2a19f68e
YisongWang <wysray@gmail.com>
2013-03-17 10:37:25 -0500

    change

--- All/addTwoLists.java
+++ All/addTwoLists.java
@@ -11,7 +11,7 @@ public class addTwoLists {
 		
 		result.intdata = value % 10;
 		LinkedListNode more = addLists(n1 == null ? null : n1.next, n2 == null ? null : n2.next, value > 10 ? 1 : 0);
-		more.setNext(more);
+		result.setNext(more);
 		
 		return result;
 	}
